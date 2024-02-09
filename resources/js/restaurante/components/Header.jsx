b641705416062de4135c1230c9b7e06c7790cb33
King0f <nicogm03@gmail.com>
2024-02-09 18:57:22 +0100

    header

--- resources/js/restaurante/components/Header.jsx
+++ resources/js/restaurante/components/Header.jsx
@@ -13,11 +13,11 @@ export const Header = ({}) => {
     <nav className="bg-gray-700  bg-opacity-5  p-4 text-white font-mono custom-nav">
         <div className="flex justify-between ">
           <div>
-            <Link to="/public/"><span className="text-3xl font-bold shadow">Los Cuatro Sentidos</span></Link>
+            <Link to="/"><span className="text-3xl font-bold shadow">Los Cuatro Sentidos</span></Link>
           </div>
           <div className="flex space-x-4">
-            <Link to="/public/login" className="mb-2 text-xl px-3 py2 rounded mt-2 shadow1 nav-link">Login</Link>
-            <Link to="/public//register" className="mb-2 text-xl px-3 py2 rounded mt-2 shadow1 nav-link">Register</Link>
+            <Link to="/login" className="mb-2 text-xl px-3 py2 rounded mt-2 shadow1 nav-link">Login</Link>
+            <Link to="/register" className="mb-2 text-xl px-3 py2 rounded mt-2 shadow1 nav-link">Register</Link>
             <a href="#" className="mb-2 text-xl px-3 py2 rounded mt-2 shadow1 nav-link">Realizar una reserva</a>
           </div>
         </div>
