e74ef87575e384b6135fb15745c71b79b10ea668
King0f <nicogm03@gmail.com>
2024-02-09 18:01:32 +0100

    scss

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -14,7 +14,7 @@
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
     <!-- Scripts -->
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/js/app.js, resources/sass/app.scss'])
 </head>
 <body>
     <div id="app">
