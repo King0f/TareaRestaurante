f7ce0c98b686822454c1c777a94dbd879c2a1abb
King0f <nicogm03@gmail.com>
2024-02-12 15:57:13 +0100

    a

---
+++ resources/js/restaurante/components/Footer.jsx
@@ -0,0 +1,16 @@
+/**
+ * La función anterior es un componente de React que representa un elemento de pie de página con algo
+ * de texto.
+ * @returns El componente Pie de página devuelve un elemento de pie de página con una clase de
+ * "bg-black p-4 opacity-90 text-white text-center" y el texto "Pagina creada por Nicolas Gomez
+ * Mulero".
+ */
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <footer class="bg-black p-4 opacity-90 text-white text-center">
+    Pagina creada por Nicolas Gomez Mulero
+</footer>
+  )
+}
