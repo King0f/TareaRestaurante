d54aeb00510f66ab61a9cf7518dafec5523bbf5b
King0f <nicogm03@gmail.com>
2024-02-12 16:02:21 +0100

    a

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './resources/js/restaurante/*.jsx',
+    './resources/js/restaurante/**/*.jsx',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
