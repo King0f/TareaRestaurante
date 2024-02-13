af0ccc5eb9ec4fe5a99c3c6ba87591ebbdf9d7e6
King0f <nicogm03@gmail.com>
2024-02-13 16:45:10 +0100

    dsfsdfsf

--- resources/js/restaurante/pages/Calenndar.jsx
+++ resources/js/restaurante/pages/Calenndar.jsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useNavigate } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom'; // Importa useNavigate desde react-router-dom
 import FullCalendar from '@fullcalendar/react';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import timeGridPlugin from '@fullcalendar/timegrid';
@@ -6,32 +7,35 @@ import interactionPlugin from '@fullcalendar/interaction';
 
 function Calenndar() {
     const [events, setEvents] = useState([]);
-    const navigate = useNavigate();
+    const navigate = useNavigate(); 
+
     useEffect(() => {
-        // Asume que tienes una función para obtener los eventos desde tu backend
         fetchEvents().then((events) => {
             const formattedEvents = events.map((event) => ({
                 title: event.Horas_Disponibles, 
-                start: event.Dias_Disponibles, 
-                url: navigate('/restaurante/realizarReserva/' + event.Dias_Disponibles + '/' + event.Horas_Disponibles)
+                start: event.Dias_Disponibles,
             }));
 
             setEvents(formattedEvents);
         });
     }, []);
 
+    const handleEventClick = (clickInfo) => {
+        const { event } = clickInfo;
+        navigate(`/restaurante/realizarReserva/${event.startStr}/${event.title}`);
+    };
+
     return (
         <FullCalendar
             plugins={[dayGridPlugin, timeGridPlugin, interactionPlugin]}
             initialView="dayGridMonth"
             events={events}
+            eventClick={handleEventClick} 
         />
     );
 }
 
 async function fetchEvents() {
-    // Implementa la lógica de fetching de tus eventos aquí,
-    // por ejemplo, usando fetch para obtener los datos de tu API.
     const response = await fetch('/api/eventosCalendario');
     const data = await response.json();
     return data;
