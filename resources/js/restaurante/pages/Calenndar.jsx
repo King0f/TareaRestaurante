e8463a9d4ea67b32645833a02e7e4615b9b16523
King0f <nicogm03@gmail.com>
2024-02-13 16:13:59 +0100

    sdfsfs

--- resources/js/restaurante/pages/Calenndar.jsx
+++ resources/js/restaurante/pages/Calenndar.jsx
@@ -13,7 +13,7 @@ function Calenndar() {
             const formattedEvents = events.map((event) => ({
                 title: event.Horas_Disponibles, 
                 start: event.Dias_Disponibles, // '2024-02-14' por ejemplo, FullCalendar interpretará correctamente esta cadena
-                url: "/realizarReserva/" + event.Dias_Disponibles + "/" + event.Horas_Disponibles 
+                url: "/restaurante/realizarReserva/" + event.Dias_Disponibles + "/" + event.Horas_Disponibles 
             }));
 
             setEvents(formattedEvents);
