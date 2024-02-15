1acbef6dab7a9a701f58ac60d3088bdbe910e60b
King0f <nicogm03@gmail.com>
2024-02-15 19:44:20 +0100

    dggdgdfsg

--- resources/js/restaurante/pages/MiCuenta.jsx
+++ resources/js/restaurante/pages/MiCuenta.jsx
@@ -116,7 +116,77 @@ function MiCuenta (){
  return (
     <>
     <div>Hola</div>
-    
+    <h2>Datos del usuario</h2>
+    <p><b>Nombre:</b> {user.name}</p>
+    <p><b>Apellidos:</b> {user.apellido}</p>
+    <p><b>Teléfono:</b> {user.telefono}</p>
+    <p><b>Alergias:</b> {user.alergias}</p>
+    <p><b>Email:</b> {user.email}</p>
+    <h2>Tarjetas de credito guardadas:</h2>
+    <ul>
+    {tarjetas?.map((tarjeta) => (
+        <>
+        <li>
+                <span>Número de la tarjeta: {tarjeta.Nº_Tarjeta}</span>
+                <span>Fecha de caducidad: {tarjeta.Fecha_Caducidad}</span>
+                <span>CVV: {tarjeta.CVV}</span>
+                <button className="text-black" onClick={() => handleBorrar(reserva.id)}>Borrar la reserva</button>
+        </li>
+        
+        </>
+              ))}
+    </ul>
+    <button onClick={toggleFormulario} className="text-black">
+                Añadir tarjeta a la cuenta
+    </button>
+    {mostrarFormulario && (
+                <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                     <label htmlFor="Nº_Tarjeta" className="block text-gray-700 text-sm font-bold mb-2">
+                       Nº de la tarjeta:
+                     </label>
+                     <input
+                       type="text"
+                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                       id="Nº_Tarjeta"
+                       name="Nº_Tarjeta"
+                       placeholder= "Inserte el número de la tarjeta"
+                       onChange={(e) => setNTarjeta(e.target.value)}
+                     />
+                   </div>
+                   <div className="mb-4">
+                     <label htmlFor="Fecha_Caducidad" className="block text-gray-700 text-sm font-bold mb-2">
+                       Fecha de caducidad:
+                     </label>
+                     <input
+                       type="text"
+                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                       id="Fecha_Caducidad"
+                       name="Fecha_Caducidad"
+                       placeholder= "Inserte el número de la tarjeta"
+                       onChange={(e) => setFechaCaducidad(e.target.value)}
+                     />
+                   </div>
+                   <div className="mb-4">
+                     <label htmlFor="CVV" className="block text-gray-700 text-sm font-bold mb-2">
+                       CVV:
+                     </label>
+                     <input
+                       type="text"
+                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                       id="CVV"
+                       name="CVV"
+                       placeholder= "Inserte el número de la tarjeta"
+                       onChange={(e) => setCvv(e.target.value)}
+                     />
+                   </div>
+                   <div className="flex items-center justify-between">
+                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                       Añadir Tarjeta
+                     </button>
+                   </div>
+                 </form>
+            )}
     </>
  )
 }
