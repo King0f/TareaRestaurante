9abd0fb53f92e6fe07d22215d8d2459d9229263f
King0f <nicogm03@gmail.com>
2024-02-15 14:02:48 +0100

    sfasf

--- app/Models/horario.php
+++ app/Models/horario.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Horario extends Model
+class horario extends Model
 {
     use HasFactory;
 
