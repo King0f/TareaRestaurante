5b27c45cef2da0ce88fb7c06edb922219c3cc64c
King0f <nicogm03@gmail.com>
2024-02-15 18:27:09 +0100

    asdasd

--- resources/js/restaurante/pages/MiCuenta.jsx
+++ resources/js/restaurante/pages/MiCuenta.jsx
@@ -4,6 +4,10 @@ import { Link } from "react-router-dom";
 function MiCuenta (){
     const [user,setUser] = useState();
     const [tarjetas, setTarjetas] = useState([]);
+    const [mostrarFormulario, setMostrarFormulario] = useState(false);
+    const [n_tarjeta, setNTarjeta] = useState();
+    const [fecha_caducidad, setFechaCaducidad] = useState();
+    const [cvv, setCvv] = useState(); // Nuevo estado para controlar la visibilidad del formulario
     useEffect(() => {
         const obtenerDatosUsuario = async () => {
           const token = localStorage.getItem('token');
@@ -52,7 +56,39 @@ function MiCuenta (){
           }).catch(error => {
             console.error('Error al obtener las tarjetas:', error);
           });
-    },[])
+
+          
+    },[tarjetas])
+
+    const toggleFormulario = () => {
+      setMostrarFormulario(!mostrarFormulario);
+  };
+    const handleSubmit = async (e) => {
+      e.preventDefault();
+      const token = localStorage.getItem('token');
+        try {
+          const respuesta = await fetch('/api/addTarjeta', {
+            method: 'POST', 
+            headers: {
+              'Content-Type': 'application/json',
+              'Authorization': `Bearer ${token}`,
+            },
+            body: JSON.stringify({
+              N_Tarjeta: n_tarjeta,
+              fecha_caducidad: fecha_caducidad,
+              cvv: cvv,
+            }), 
+          });
+
+          if (respuesta.ok) {
+            
+          }
+        } catch (error) {
+          console.error('Error al enviar los datos:', error);
+        }
+        // Opcional: Ocultar el formulario después de enviar
+        setMostrarFormulario(false);
+  };
     if (!user) {
         return <p>Cargando detalles del usuario...</p>;
       }
@@ -77,9 +113,57 @@ function MiCuenta (){
         </>
               ))}
     </ul>
-    <Link to="/restaurante/addTarjeta">
-    <button className="text-black">Añadir tarjeta a la cuenta</button>
-    </Link>
+    <button onClick={toggleFormulario} className="text-black">
+                Añadir tarjeta a la cuenta
+    </button>
+    {mostrarFormulario && (
+                <form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                     <label htmlFor="Nº_Tarjeta" className="block text-gray-700 text-sm font-bold mb-2">
+                       Nº de la tarjeta:
+                     </label>
+                     <input
+                       type="text"
+                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                       id="Nº_Tarjeta"
+                       name="Nº_Tarjeta"
+                       placeholder= "Inserte el número de la tarjeta"
+                       onChange={(e) => setNTarjeta(e.target.value)}
+                     />
+                   </div>
+                   <div className="mb-4">
+                     <label htmlFor="Fecha_Caducidad" className="block text-gray-700 text-sm font-bold mb-2">
+                       Fecha de caducidad:
+                     </label>
+                     <input
+                       type="text"
+                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                       id="Fecha_Caducidad"
+                       name="Fecha_Caducidad"
+                       placeholder= "Inserte el número de la tarjeta"
+                       onChange={(e) => setFechaCaducidad(e.target.value)}
+                     />
+                   </div>
+                   <div className="mb-4">
+                     <label htmlFor="CVV" className="block text-gray-700 text-sm font-bold mb-2">
+                       CVV:
+                     </label>
+                     <input
+                       type="text"
+                       className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                       id="CVV"
+                       name="CVV"
+                       placeholder= "Inserte el número de la tarjeta"
+                       onChange={(e) => setCvv(e.target.value)}
+                     />
+                   </div>
+                   <div className="flex items-center justify-between">
+                     <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+                       Añadir Tarjeta
+                     </button>
+                   </div>
+                 </form>
+            )}
     </>
  )
 }
