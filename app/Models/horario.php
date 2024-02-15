adaff3c1af2afbbb7d22a7cdc15d130685736d5f
King0f <nicogm03@gmail.com>
2024-02-15 13:58:08 +0100

    ADASD

--- app/Models/horario.php
+++ app/Models/horario.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class horario extends Model
+class Horario extends Model
 {
     use HasFactory;
 
