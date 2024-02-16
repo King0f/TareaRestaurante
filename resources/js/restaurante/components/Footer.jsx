c8e8cc70ca8cb89aa4d231cb00004f7ed6b389c2
King0f <nicogm03@gmail.com>
2024-02-16 18:43:25 +0100

    afasfa

--- resources/js/restaurante/components/Footer.jsx
+++ resources/js/restaurante/components/Footer.jsx
@@ -3,26 +3,24 @@ import { FaTwitter, FaFacebook, FaInstagram, FaLinkedinIn } from 'react-icons/fa
 
 export const Footer = () => {
   return (
-    <footer className="bg-black p-8 opacity-95 text-white text-center">
-      <div className="max-w-4xl mx-auto flex items-center space-y-4">
-        <div className="flex justify-center space-x-4">
-          <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-            <FaTwitter size="24" />
-          </a>
-          <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-            <FaFacebook size="24" />
-          </a>
-          <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-            <FaInstagram size="24" />
-          </a>
-          <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
-            <FaLinkedinIn size="24" />
-          </a>
-        </div>
+    <footer className="bg-black p-8 opacity-95 text-white text-center flex flex-col items-center justify-center">
+      <div className="flex justify-center space-x-4 mb-4">
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+          <FaTwitter size="24" />
+        </a>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+          <FaFacebook size="24" />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+          <FaInstagram size="24" />
+        </a>
+        <a href="https://linkedin.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
+          <FaLinkedinIn size="24" />
+        </a>
       </div>
-      <div className="mt-4 text-sm opacity-80">
+      <div className="text-sm opacity-80">
         © {new Date().getFullYear()} Todos los derechos reservados. Página creada por Nicolás Gómez Mulero.
       </div>
     </footer>
-  )
-}
+  );
+};
