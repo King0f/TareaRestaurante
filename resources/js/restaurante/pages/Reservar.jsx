05c125f80fcf4dadda52067324710822f4fc45cb
King0f <nicogm03@gmail.com>
2024-02-16 13:51:20 +0100

    sadadad

--- resources/js/restaurante/pages/Reservar.jsx
+++ resources/js/restaurante/pages/Reservar.jsx
@@ -120,8 +120,7 @@ const handleSubmit = async (e) => {
     <Header2 />
     {isLoggedIn ? (
       <div className="min-h-screen bg-gray-900 flex justify-center items-center">
-      <br/>
-      <div className="bg-white p-8 rounded-lg shadow-md w-[80rem]">
+      <div className="bg-white p-8 rounded-lg shadow-md w-[80rem] mx-4 my-8">
       <h2 className="text-center text-3xl font-semibold mb-4 text-gray-800 pt-4">Datos de la reserva</h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
@@ -282,7 +281,7 @@ const handleSubmit = async (e) => {
     ) : (
         <RegularForm fecha={fecha} hora={hora}/>
     )}
-    <br/>
+    
     <Footer />
     </>
   );
