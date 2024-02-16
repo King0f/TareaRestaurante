fd592caee74e8fcca7a87fa32c835bf3f3aedc59
King0f <nicogm03@gmail.com>
2024-02-16 13:02:03 +0100

    sasdasdsad

--- resources/js/restaurante/components/Header.jsx
+++ resources/js/restaurante/components/Header.jsx
@@ -12,7 +12,7 @@ export const Header = ({}) => {
   const isLoggedIn = !!localStorage.getItem('token');
   const cerrarSesion = () => {
     localStorage.removeItem('token');
-    window.location.reload();
+    window.location.href = '/restaurante/';
   }
   return (
     <>
