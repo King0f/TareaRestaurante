56b6d6345d5a6daa59b689d204ad5bc1e8008ba2
King0f <nicogm03@gmail.com>
2024-02-16 18:07:50 +0100

    adasdsad

--- resources/js/restaurante/pages/ReservaRealizada.jsx
+++ resources/js/restaurante/pages/ReservaRealizada.jsx
@@ -12,11 +12,11 @@ function ReservaRealizada() {
     return (
       <>
         <Header2/>
-        <div className="flex flex-col justify-center items-center min-h-screen bg-gray-100">
+        <div className="flex flex-col justify-center items-center min-h-screen bg-gray-900 pt-20">
           {isLoggedIn ? (
             <div className="text-center space-y-4">
-              <h1 className="text-4xl font-bold">Reserva realizada con éxito</h1>
-              <h2 className="text-2xl">Compruebe la página de 'Mis Reservas' para comprobar todos los datos de su reserva</h2>
+              <h1 className="text-4xl font-bold text-white">Reserva realizada con éxito</h1>
+              <h2 className="text-2xl text-gray-300">Compruebe la página de 'Mis Reservas' para comprobar todos los datos de su reserva</h2>
               <Link to={'/restaurante/misReservas'}>
                 <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Mis Reservas</button>
               </Link>
@@ -24,8 +24,8 @@ function ReservaRealizada() {
             </div> 
           ) : (
             <div className="text-center space-y-4">
-              <h1 className="text-4xl font-bold">Reserva realizada con éxito</h1>
-              <h2 className="text-2xl">Compruebe su bandeja de entrada en el correo para comprobar todos los datos de la reserva</h2>
+              <h1 className="text-4xl font-bold text-white">Reserva realizada con éxito</h1>
+              <h2 className="text-2xl text-gray-300">Compruebe su bandeja de entrada en el correo para comprobar todos los datos de la reserva</h2>
               <button className="mt-4 bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded" onClick={volverAlInicio}>Volver al inicio</button>
             </div> 
           )}
