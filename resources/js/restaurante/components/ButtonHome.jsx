bb579b9ad2e7d69cd881b4cdc5a3a12a70a0cc21
King0f <nicogm03@gmail.com>
2024-02-16 13:08:14 +0100

    zxczxc

--- resources/js/restaurante/components/ButtonHome.jsx
+++ resources/js/restaurante/components/ButtonHome.jsx
@@ -1,18 +1,15 @@
-/**
- * El componente ButtonHome es un botón sobre el que se puede pasar el cursor y activa eventos
- * onMouseEnter y onMouseLeave.
- * @returns El código devuelve un componente de botón con el texto "Realizar una reserva". El botón
- * tiene un nombre de clase que incluye "text-white", "font-bold", "py-3", "px-6", "rounded" y "mt-4".
- * El nombre de clase también incluye "flotado" si la propiedad "flotado" es verdadera. El botón tiene
- * controladores de eventos onMouseEnter y onMouseLeave que son
- */
 import React from 'react'
-
+import { useNavigate } from 'react-router-dom'
 export const ButtonHome = ({onMouseEnter, onMouseLeave, hovered}) => {
+  const navigate = useNavigate()
+  function handleClick(){
+    navigate('/restaurante/realizarReserva')
+  }
   return (
     <button className="botonInicio text-white font-bold py-3 px-6 rounded mt-4 ${hovered ? 'hovered' : ''}"
     onMouseEnter={onMouseEnter}
     onMouseLeave={onMouseLeave}
+    onClick={handleClick}
       >
           Realizar una reserva
     </button>
