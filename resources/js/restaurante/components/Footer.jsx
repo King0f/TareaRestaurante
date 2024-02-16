cd46d0210bad3cf34bdd1d0c327cac3e857d97d2
King0f <nicogm03@gmail.com>
2024-02-16 18:38:48 +0100

    sdsdf

--- resources/js/restaurante/components/Footer.jsx
+++ resources/js/restaurante/components/Footer.jsx
@@ -5,9 +5,6 @@ export const Footer = () => {
   return (
     <footer className="bg-black p-8 opacity-95 text-white text-center">
       <div className="max-w-4xl mx-auto flex flex-col md:flex-row justify-between items-center space-y-4">
-        <div className='text-center'>
-          Página creada por Nicolás Gómez Mulero
-        </div>
         <div className="flex justify-center space-x-4">
           <a href="https://twitter.com" target="_blank" rel="noopener noreferrer" className="text-white hover:text-gray-400">
             <FaTwitter size="24" />
@@ -24,7 +21,7 @@ export const Footer = () => {
         </div>
       </div>
       <div className="mt-4 text-sm opacity-80">
-        © {new Date().getFullYear()} Todos los derechos reservados.
+        © {new Date().getFullYear()} Todos los derechos reservados. Página creada por Nicolás Gómez Mulero.
       </div>
     </footer>
   )
